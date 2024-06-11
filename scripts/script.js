bfb6cf2f7c988419f9b0b6c60fbe3aaa5f345e60
Nathalia-Seki <nathalia.seki@gmail.com>
2024-06-10 23:13:08 -0300

    continuação avaliação

--- scripts/script.js
+++ scripts/script.js
@@ -15,29 +15,23 @@
 //     `
 // })
 
-var teste1 = []
+var filmesIndex = []
 
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
-teste1.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
+filmesIndex.push(new FilmesEmCartaz("Divertidamente 2","07/06/2024","images/Filmes/divertida-mente-2-.jpg"))
+filmesIndex.push(new FilmesEmCartaz("Furiosa","07/06/2024","images/Filmes/Furiosa.jpg"))
+filmesIndex.push(new FilmesEmCartaz("Garfield","07/06/2024","images/Filmes/garfild.jpg"))
 
 
 
-teste1.forEach((elemento) => {
-    document.querySelector("#teste1").innerHTML += `
+
+filmesIndex.forEach((elemento) => {
+    document.querySelector("#filmesIndex").innerHTML += `
         <div class="card" style="width: 18rem;">
-            <img src="${elemento.img}" class="card-img-top" alt="...">
-            <div class="card-body">
+            <img src="${elemento.img}" class="card-img-top h-75" alt="...">
+            <div class="card-body text-center">
                 <h5 class="card-title">${elemento.nome}</h5>
                 <p class="card-text">${elemento.data}</p>
-                <button type="button" href="#" class="btn btn-primary">SINOPSE</button>
+                <a class="btn btn-dark" href="em_cartaz.html" role="button">VER MAIS</a>
             </div>
         </div>
     `
